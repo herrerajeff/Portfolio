e14e8f36afa99a460ae08da0c477d98ba8648155
Jeff Herrera <herrera.jeffrey@gmail.com>
2018-09-17 23:27:56 -0400

    Initial Commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   siteUrl: 'https://www.jeffreyherrera.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteBanner: 'src/images/profile.jpg',
+  siteBanner: '/profile.jpg',
   siteDescription: 'The Portfolio of Jeffrey Herrera',
 
   siteFBAppID: '123456789', // Facebook App ID
