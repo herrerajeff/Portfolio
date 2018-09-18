550f8944434d1244cbb82ab2e05fcb13d905238e
Jeff Herrera <herrera.jeffrey@gmail.com>
2018-09-17 23:50:54 -0400

    Initial Commit

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,66 +4,6 @@ import PropTypes from 'prop-types';
 import config from '../utils/config';
 
 const SEO = props => {
-  const { postNode, postPath, postSEO } = props;
-  let title;
-  let description;
-  let image;
-  let postURL;
-  const realPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
-  if (postSEO) {
-    // const postMeta = postNode.childMarkdownRemark;
-    title = postNode.title; // eslint-disable-line prefer-destructuring
-    description = postNode.description.description;
-    image = config.siteBanner;
-    postURL = config.siteUrl + realPrefix + postPath;
-  } else {
-    title = config.siteTitle;
-    description = config.siteDescription;
-    image = config.siteBanner;
-  }
-  image = config.siteUrl + realPrefix + image;
-  const blogURL = config.siteUrl + config.pathPrefix;
-  const schemaOrgJSONLD = [
-    {
-      '@context': 'http://schema.org',
-      '@type': 'WebSite',
-      url: blogURL,
-      name: title,
-      alternateName: config.siteTitleAlt ? config.siteTitleAlt : '',
-    },
-  ];
-  if (postSEO) {
-    schemaOrgJSONLD.push(
-      {
-        '@context': 'http://schema.org',
-        '@type': 'BreadcrumbList',
-        itemListElement: [
-          {
-            '@type': 'ListItem',
-            position: 1,
-            item: {
-              '@id': postURL,
-              name: title,
-              image,
-            },
-          },
-        ],
-      },
-      {
-        '@context': 'http://schema.org',
-        '@type': 'BlogPosting',
-        url: blogURL,
-        name: title,
-        alternateName: config.siteTitleAlt ? config.siteTitleAlt : '',
-        headline: title,
-        image: {
-          '@type': 'ImageObject',
-          url: image,
-        },
-        description,
-      }
-    );
-  }
   return (
     <Helmet titleTemplate="%s · Jeffrey Herrera" defaultTitle="Jeffrey Herrera">
       <meta charSet="utf-8" />
@@ -74,21 +14,21 @@ const SEO = props => {
         name="keywords"
         content="graphic, design, portfolio, jeffrey, herrera, jeff, graphics, designer, minimalism, bold, typography, motion, web, development, product"
       />
-      <meta name="image" content={image} />
+      <meta name="image" content={config.siteUrl + realPrefix + config.siteBanner} />
       <script type="application/ld+json">{JSON.stringify(schemaOrgJSONLD)}</script>
       <meta property="og:locale" content="de_DE" />
       <meta property="og:site_name" content={config.ogSiteName} />
       <meta property="og:url" content={config.siteUrl} />
       <meta property="og:title" content={config.siteTitle} />
       <meta property="og:description" content={config.siteDescription} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={config.siteUrl + realPrefix + config.siteBanner} />
       <meta property="fb:app_id" content={config.siteFBAppID ? config.siteFBAppID : ''} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ''} />
       <meta name="twitter:title" content={config.siteTitle} />
       <meta name="twitter:url" content={config.siteUrl} />
       <meta name="twitter:description" content={config.siteDescription} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={config.siteUrl + realPrefix + config.siteBanner} />
       <meta name="apple-mobile-web-app-title" content="jeffreyherrera.com" />
       <meta name="application-name" content="jeffreyherrera.com" />
       <meta name="theme-color" content="#fcda00" />
