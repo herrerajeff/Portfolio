2d6be4a7f4a915a8697c3d2b5f92bfd6c598db42
Jeff Herrera <herrera.jeffrey@gmail.com>
2018-09-17 23:43:49 -0400

    Initial Commit

--- src/components/seo.js
+++ src/components/seo.js
@@ -80,15 +80,15 @@ const SEO = props => {
       <meta property="og:site_name" content={config.ogSiteName} />
       <meta property="og:url" content={postSEO ? postURL : blogURL} />
       {postSEO ? <meta property="og:type" content="article" /> : null}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta property="og:title" content={config.siteTitle} />
+      <meta property="og:description" content={config.siteDescription} />
       <meta property="og:image" content={image} />
       <meta property="fb:app_id" content={config.siteFBAppID ? config.siteFBAppID : ''} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ''} />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={config.siteTitle} />
       <meta name="twitter:url" content={config.siteUrl} />
-      <meta name="twitter:description" content={description} />
+      <meta name="twitter:description" content={config.siteDescription} />
       <meta name="twitter:image" content={image} />
       <meta name="apple-mobile-web-app-title" content="jeffreyherrera.com" />
       <meta name="application-name" content="jeffreyherrera.com" />
