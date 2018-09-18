273eb5477ac8c047339c22a370aa93d9ca003578
Jeff Herrera <herrera.jeffrey@gmail.com>
2018-09-18 00:29:51 -0400

    Initial Commit

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,47 +4,103 @@ import PropTypes from 'prop-types';
 import config from '../utils/config';
 
 const SEO = props => {
-  const realPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+  const { postNode, postPath, postSEO } = this.props;
+    let title;
+    let description;
+    let image;
+    let postURL;
+    if (postSEO) {
+      const postMeta = postNode;
+      ({ title } = postMeta);
+      description = postMeta.description.description
+        ? postMeta.description.description
+        : postNode.excerpt;
+      image = postMeta.cover;
+      postURL = urljoin(config.siteUrl, config.pathPrefix, postPath);
+    } else {
+      title = config.siteTitle;
+      description = config.siteDescription;
+      image = config.siteLogo;
+    }
+
+    image = urljoin(config.siteUrl, config.pathPrefix, image);
+    const blogURL = urljoin(config.siteUrl, config.pathPrefix);
+    const schemaOrgJSONLD = [
+      {
+        "@context": "http://schema.org",
+        "@type": "WebSite",
+        url: blogURL,
+        name: title,
+        alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
+      }
+    ];
+    if (postSEO) {
+      schemaOrgJSONLD.push(
+        {
+          "@context": "http://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              item: {
+                "@id": postURL,
+                name: title,
+                image
+              }
+            }
+          ]
+        },
+        {
+          "@context": "http://schema.org",
+          "@type": "BlogPosting",
+          url: blogURL,
+          name: title,
+          alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
+          headline: title,
+          image: {
+            "@type": "ImageObject",
+            url: image
+          },
+          description
+        }
+      );
+      }
   return (
     <Helmet titleTemplate="%s · Jeffrey Herrera" defaultTitle="Jeffrey Herrera">
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1, width=device-width" />
       <title>{config.siteTitle}</title>
-      <meta name="description" content={config.siteDescription} />
-      <meta
-        name="keywords"
-        content="graphic, design, portfolio, jeffrey, herrera, jeff, graphics, designer, minimalism, bold, typography, motion, web, development, product"
-      />
-      <meta name="image" content={config.siteUrl + realPrefix + config.siteBanner} />
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content={config.ogSiteName} />
-      <meta property="og:url" content={config.siteUrl} />
-      <meta property="og:title" content={config.siteTitle} />
-      <meta property="og:description" content={config.siteDescription} />
-      <meta property="og:image" content={config.siteUrl + realPrefix + config.siteBanner} />
-      <meta property="fb:app_id" content={config.siteFBAppID ? config.siteFBAppID : ''} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ''} />
-      <meta name="twitter:title" content={config.siteTitle} />
-      <meta name="twitter:url" content={config.siteUrl} />
-      <meta name="twitter:description" content={config.siteDescription} />
-      <meta name="twitter:image" content={config.siteUrl + realPrefix + config.siteBanner} />
-      <meta name="apple-mobile-web-app-title" content="jeffreyherrera.com" />
-      <meta name="application-name" content="jeffreyherrera.com" />
-      <meta name="theme-color" content="#fcda00" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fcda00" />
+      {/* General tags */}
+      <meta name="description" content={description} />
+        <meta name="image" content={image} />
+        {/* Schema.org tags */}
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgJSONLD)}
+        </script>
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={postSEO ? postURL : blogURL} />
+        {postSEO ? <meta property="og:type" content="article" /> : null}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta
+          property="fb:app_id"
+          content={config.siteFBAppID ? config.siteFBAppID : ""}
+        />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:creator"
+          content={config.userTwitter ? config.userTwitter : ""}
+        />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
     </Helmet>
   );
 };
 
 export default SEO;
-
-SEO.propTypes = {
-  postNode: PropTypes.object,
-  postPath: PropTypes.string,
-  postSEO: PropTypes.bool,
-};
