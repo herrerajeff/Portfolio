7a7b8875c3bc95f875c8ebf1f6a47c5947edf8c8
Jeff Herrera <herrera.jeffrey@gmail.com>
2018-09-19 11:44:17 -0400

    Initial Commit

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
-import PropTypes from 'prop-types';
+import Helmet, { HelmetProvider } from 'react-helmet-async';
 import config from '../utils/config';
 
 const SEO = props => {
@@ -15,30 +14,32 @@ const SEO = props => {
     },
   ];
   return (
-    <Helmet titleTemplate="%s · Jeffrey Herrera" defaultTitle="Jeffrey Herrera">
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <meta name="description" content={config.siteDescription} />
-      <meta name="image" content={config.siteUrl + config.siteBanner} />
-      <script type="application/ld+json">
-        {JSON.stringify(schemaOrgJSONLD)}
-      </script>
-      {postPath ? (<meta property="og:url" content={postPath}/>):(<meta property="og:url" content={config.siteUrl}/>)};
-      <meta property="og:type" content="website" />
-      {title ? (<meta property="og:title" content={title} />):(<meta property="og:title" content={config.siteTitle} />)};
-      {description ? (<meta property="og:description" content={description} />):(<meta property="og:description" content={config.siteDescription} />)};
-      {image ? (<meta property="og:image" content={image} />):(<meta property="og:image" content={config.siteUrl + config.siteBanner} />)}
+    <HelmetProvider>
+      <Helmet titleTemplate="%s · Jeffrey Herrera" defaultTitle="Jeffrey Herrera">
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="description" content={config.siteDescription} />
+        <meta name="image" content={config.siteUrl + config.siteBanner} />
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgJSONLD)}
+        </script>
+        {postPath ? (<meta property="og:url" content={postPath}/>):(<meta property="og:url" content={config.siteUrl}/>)};
+        <meta property="og:type" content="website" />
+        {title ? (<meta property="og:title" content={title} />):(<meta property="og:title" content={config.siteTitle} />)};
+        {description ? (<meta property="og:description" content={description} />):(<meta property="og:description" content={config.siteDescription} />)};
+        {image ? (<meta property="og:image" content={image} />):(<meta property="og:image" content={config.siteUrl + config.siteBanner} />)}
 
-        {/* Twitter Card tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:creator"
-          content={config.userTwitter ? config.userTwitter : ""}
-        />
-        {title ? (<meta name="twitter:title" content={title} />):(<meta name="twitter:title" content={config.siteTitle} />)};
-        {description ? (<meta name="twitter:description" content={description} />):(<meta name="twitter:description" content={config.siteDescription} />)};
-        {image ? (<meta name="twitter:image" content={image} />):(<meta name="twitter:image" content={config.siteUrl + config.siteBanner} />)};
-    </Helmet>
+          {/* Twitter Card tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:creator"
+            content={config.userTwitter ? config.userTwitter : ""}
+          />
+          {title ? (<meta name="twitter:title" content={title} />):(<meta name="twitter:title" content={config.siteTitle} />)};
+          {description ? (<meta name="twitter:description" content={description} />):(<meta name="twitter:description" content={config.siteDescription} />)};
+          {image ? (<meta name="twitter:image" content={image} />):(<meta name="twitter:image" content={config.siteUrl + config.siteBanner} />)};
+      </Helmet>
+    </HelmetProvider>
   );
 };
 
